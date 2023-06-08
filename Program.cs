33fca64d7d03af616884337f71c05fb45d4ace1b
Aleksey <nighttom@mail.ru>
2023-06-08 17:38:37 +0700

    zadacha 56

--- Program.cs
+++ Program.cs
@@ -8,6 +8,76 @@
 // 9 5 3 2
 // 8 4 4 2
 
+// int[,] Create2DRandomArray(int rows, int columns, int minValue, int maxValue)
+// {
+//     int[,] array = new int[rows, columns];
+//     for (int i = 0; i < rows; i++)
+//     {
+//         for (int j = 0; j < columns; j++)
+//         {
+//             array[i, j] = new Random().Next(minValue, maxValue);
+//         }
+//     }
+//     return array;
+// }
+
+// void Print2DArray(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             Console.Write(array[i, j]+" ");
+//         }
+//         Console.WriteLine();
+//     }
+//     Console.WriteLine();
+// }
+
+// void Decreasing(int[,] array)
+// {
+//     for (int i = 0; i < array.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < array.GetLength(1); j++)
+//         {
+//             for (int k = 0; k < array.GetLength(1) - 1; k++)
+//             {   
+//                 if (array[i, k] < array[i, k + 1])
+//                 {
+//                     int temp = array[i, k + 1];
+//                     array[i, k + 1] = array[i, k];
+//                     array[i, k] = temp;
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// Console.WriteLine("Input number of rows: ");
+// int rows = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input number of columns: ");
+// int columns = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input min value array of element: ");
+// int minValue = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Input max value array of element: ");
+// int maxValue = Convert.ToInt32(Console.ReadLine());
+
+// int[,] myArray = Create2DRandomArray(rows, columns, minValue, maxValue);
+
+// Print2DArray(myArray);
+// Console.WriteLine();
+// Decreasing(myArray);
+// Print2DArray(myArray);
+
+
+// Задача 56: Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
 int[,] Create2DRandomArray(int rows, int columns, int minValue, int maxValue)
 {
     int[,] array = new int[rows, columns];
@@ -34,25 +104,30 @@ void Print2DArray(int[,] array)
     Console.WriteLine();
 }
 
-void Decreasing(int[,] array)
+int SumLine(int[,] array, int i)
 {
-    for (int i = 0; i < array.GetLength(0); i++)
-    {
-        for (int j = 0; j < array.GetLength(1); j++)
+    int sum = array[i,0];
+    for (int j = 1; j < array.GetLength(1); j++)
         {
-            for (int k = 0; k < array.GetLength(1) - 1; k++)
-            {   
-                if (array[i, k] < array[i, k + 1])
-                {
-                    int temp = array[i, k + 1];
-                    array[i, k + 1] = array[i, k];
-                    array[i, k] = temp;
-                }
-            }
+            sum += array[i,j];
         }
-    }
+        return sum;
 }
 
+int MinSumLine(int[,] array)
+{
+    int minSum = 1;
+    int sum = SumLine(array, 0);
+    for (int i = 1; i < array.GetLength(0); i++)
+    {
+        if (sum > SumLine(array, i))
+        {
+            sum = SumLine(array, i);
+            minSum = i + 1;
+        }
+    }
+    return minSum;
+}
 Console.WriteLine("Input number of rows: ");
 int rows = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine("Input number of columns: ");
@@ -65,6 +140,5 @@ int maxValue = Convert.ToInt32(Console.ReadLine());
 int[,] myArray = Create2DRandomArray(rows, columns, minValue, maxValue);
 
 Print2DArray(myArray);
-Console.WriteLine();
-Decreasing(myArray);
-Print2DArray(myArray);
+Console.WriteLine("Min sum line: " + MinSumLine(myArray));
+
